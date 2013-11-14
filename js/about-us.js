cf251512a0ce84f51f134f0647293d92f9bedb37
u-jo <lee.yuzhou@gmail.com>
2013-11-13 23:49:44 -0500

    team pictures

---
+++ js/about-us.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+	
+	$('#president').hover(function() {
+  		$(this).attr('src', 'img/mugshots/drew_fun.png');
+	}, function() {
+  		$(this).attr('src', 'img/mugshots/drew.png');
+	});
+
+	$('#membership_2').hover(function() {
+  		$(this).attr('src', 'img/mugshots/daisygreen_fun.png');
+	}, function() {
+  		$(this).attr('src', 'img/mugshots/daisygreen.png');
+	});
+
+	$('#fall_producer').hover(function() {
+  		$(this).attr('src', 'img/mugshots/morgan_fun.png');
+	}, function() {
+  		$(this).attr('src', 'img/mugshots/morgan.png');
+	});
+
+	$('#winter_producer').hover(function() {
+  		$(this).attr('src', 'img/mugshots/tyler_fun.png');
+	}, function() {
+  		$(this).attr('src', 'img/mugshots/tyler.png');
+	});
+
+	$('#technical').hover(function() {
+  		$(this).attr('src', 'img/mugshots/zack_fun.png');
+	}, function() {
+  		$(this).attr('src', 'img/mugshots/zack.png');
+	});
+
+	$('#publicity_1').hover(function() {
+  		$(this).attr('src', 'img/mugshots/andrew_fun.png');
+	}, function() {
+  		$(this).attr('src', 'img/mugshots/andrew.png');
+	});
+
+	$('#publicity_2').hover(function() {
+  		$(this).attr('src', 'img/mugshots/taylor_fun.png');
+	}, function() {
+  		$(this).attr('src', 'img/mugshots/taylor.png');
+	});
+
+	$('#playreading').hover(function() {
+  		$(this).attr('src', 'img/mugshots/elena_fun.png');
+	}, function() {
+  		$(this).attr('src', 'img/mugshots/elena.png');
+	});
+});
